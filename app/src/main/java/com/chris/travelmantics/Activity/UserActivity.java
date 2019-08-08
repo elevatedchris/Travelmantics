68d0b399cad03d9b6b9c7b479660119cf6803468
elevatedchris <elevationdeveloper@gmail>
2019-08-08 15:25:57 +0000

    Initial commit

---
+++ app/src/main/java/com/chris/travelmantics/Activity/UserActivity.java
@@ -0,0 +1,16 @@
+package com.chris.travelmantics.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chris.travelmantics.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
---
+++ app/src/main/java/com/chris/travelmantics/model/Products.java
@@ -0,0 +1,59 @@
+package com.chris.travelmantics.model;
+
+public class Products {
+
+    private String id;
+    private String title;
+    private String description;
+    private String price;
+    private String url;
+
+
+    public Products(String title, String description, String price, String url) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.url = url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
